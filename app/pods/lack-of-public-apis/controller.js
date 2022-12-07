fecef7b275480a3f9b41c89c60cf75ce3f04d890
Hagar Rachman <hagar.rachman@sage.com>
2022-12-07 15:09:53 +0200

    seperation of concerns

--- app/pods/lack-of-public-apis/controller.js
+++ app/pods/lack-of-public-apis/controller.js
@@ -1,5 +1,13 @@
-import Controller from '@ember/controller';
+import Controller from "@ember/controller";
+import { readOnly } from "@ember/object/computed";
 
 export default Controller.extend({
+  _number: readOnly("number"),
   number: Math.random(),
+
+  actions: {
+    randomize() {
+      this.set("number", Math.random());
+    },
+  },
 });
