eff58273018059dbc78fda2ce21ecb5c90c4b8b0
anna-kobrinsky-sage <anna.kobrinsky@sage.com>
2022-12-07 14:44:14 +0200

    use readOnly

--- app/pods/lack-of-public-apis/controller.js
+++ app/pods/lack-of-public-apis/controller.js
@@ -1,7 +1,8 @@
 import Controller from '@ember/controller';
-
+import { readOnly } from '@ember/object/computed';
 
 export default Controller.extend({
+  _number: readOnly("number"),
   number: Math.random(),
 
   actions: {
