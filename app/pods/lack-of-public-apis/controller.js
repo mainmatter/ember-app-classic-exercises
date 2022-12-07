0fafca9dac82935d0584452f66011941d83aeb84
anna-kobrinsky-sage <anna.kobrinsky@sage.com>
2022-12-07 14:43:33 +0200

    loose coupling

--- app/pods/lack-of-public-apis/controller.js
+++ app/pods/lack-of-public-apis/controller.js
@@ -1,5 +1,12 @@
 import Controller from '@ember/controller';
 
+
 export default Controller.extend({
   number: Math.random(),
+
+  actions: {
+    randomize() {
+      this.set('number', Math.random());
+    }
+  }
 });
