f79cec3a9f0361201ae33568eb0f44b16100365b
anna-kobrinsky-sage <anna.kobrinsky@sage.com>
2022-12-07 15:04:29 +0200

    use addon

--- app/pods/property-syntax/controller.js
+++ app/pods/property-syntax/controller.js
@@ -1,11 +1,12 @@
+import { computed } from '@ember/object';
 import Controller from '@ember/controller';
 
 export default Controller.extend({
   foo: 2,
 
-  bar: function() {
+  bar: computed('foo', function() {
     return this.get('foo') * Math.random();
-  }.property('foo'),
+  }),
 })
 
 // https://github.com/mainmatter/ember2-x-codemods
