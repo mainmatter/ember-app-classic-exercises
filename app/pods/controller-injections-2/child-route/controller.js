aba90975fb3133767092ca37112a0b7912d7b5ec
anna-kobrinsky-sage <anna.kobrinsky@sage.com>
2022-12-14 14:58:14 +0200

    inject all data from parent controller to child

--- app/pods/controller-injections-2/child-route/controller.js
+++ app/pods/controller-injections-2/child-route/controller.js
@@ -1,9 +1,12 @@
 import Controller from '@ember/controller';
 import { computed } from "@ember/object";
+import { inject as controller } from '@ember/controller';
+import { readOnly } from '@ember/object/computed';
 
 export default Controller.extend({
+  rootController: controller("controller-injections-2"),
 
-  dataOriginal: null, // Should be populated from parent route
+  dataOriginal: readOnly("rootController.data"), // Should be populated from parent route
 
   dataModified: computed('dataOriginal', function () {
     return this.get('dataOriginal') * 2;
