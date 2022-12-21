72b0989918b2c5f1b9f220a0ae702c6acc0b6095
khen-dror-sage <94138583+khen-dror-sage@users.noreply.github.com>
2022-12-21 14:31:20 +0200

    Khen & Daniel PR

--- app/pods/controller-injections-3/child-route/controller.js
+++ app/pods/controller-injections-3/child-route/controller.js
@@ -1,12 +1,11 @@
 import Controller from '@ember/controller';
 import { computed } from "@ember/object";
 import { readOnly } from '@ember/object/computed';
-import { inject as controller } from '@ember/controller';
 
 export default Controller.extend({
-  parentController: controller('controller-injections-3'),
+  model: null,
 
-  dataOriginal: readOnly('parentController.data'), // Should be populated from parent route
+  dataOriginal: readOnly('model'), // Should be populated from parent route
 
   dataModified: computed('dataOriginal', function () {
     return this.get('dataOriginal') * 2;
