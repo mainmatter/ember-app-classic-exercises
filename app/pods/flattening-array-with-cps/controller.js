894ee42ac96a7bf664bb58529a93903ac35ba3c0
khen-dror-sage <94138583+khen-dror-sage@users.noreply.github.com>
2022-12-28 14:43:10 +0200

    khen-and-daniel

--- app/pods/flattening-array-with-cps/controller.js
+++ app/pods/flattening-array-with-cps/controller.js
@@ -1,28 +1,27 @@
-import Controller from '@ember/controller';
-import { computed } from '@ember/object';
-import { readOnly } from '@ember/object/computed';
-import { inject as service } from '@ember/service';
+import Controller from "@ember/controller";
+import { computed } from "@ember/object";
+import { readOnly } from "@ember/object/computed";
+import { inject as service } from "@ember/service";
 
 export default Controller.extend({
   store: service(),
 
   model: null, // Populated from route
-  owners: readOnly('model'),
+  owners: readOnly("model"),
 
 
-  allPets: computed('owners.@each.pets', function () {
-    return this
-      .get('owners')
-      .map((owner) => owner.pets)
-      .flat()
-      .sortBy('name');
+  allPetsArrays: computed("owners.@each.pets", function () {
+    return this.get("owners").mapBy("pets");
   }),
 
+  allPets: computed("allPetsArrays.@each.[]", function () {
+    return this.get("allPetsArrays").flat().sortBy("name");
+  }),
 
   actions: {
     addPet() {
-      this.get('owners.firstObject.pets').addObject({
-        name: 'New pet',
+      this.get("owners.firstObject.pets").addObject({
+        name: "New pet",
       });
     },
   },
