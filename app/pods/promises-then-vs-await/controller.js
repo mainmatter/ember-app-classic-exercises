284a5deb5e2e7911215ad2295ad5a99201f099c4
anna-kobrinsky-sage <anna.kobrinsky@sage.com>
2023-01-25 14:32:09 +0200

    change current code to use async-await

--- app/pods/promises-then-vs-await/controller.js
+++ app/pods/promises-then-vs-await/controller.js
@@ -14,22 +14,21 @@ export default Controller.extend({
   apiService: service('promises-then-vs-await'),
 
   actions: {
-    doTheThing() {
-      return new Promise((resolve, reject) => {
-        this.get("apiService").fetchData("loginData", "createRecords", {
+    async doTheThing() {
+      const fetchedData = await this.get("apiService").fetchData("loginData", "createRecords", {
           recordType: "glbudgetheader",
           recordNo: 123,
           data: "chunk",
           budgetId: "selectedRoot.absoluteRoot.id",
           exportType: "model.exportSelection",
           selectedSIbudget: "model.intacctBudgetId",
-        }).then((data) => {
-          if (typeof data === "string") {
-            // wait for the job to complete
-            waitForJob(data).then(resolve, reject);
-          }
-        }).then(alert);
-      });
+        });
+          
+      if (typeof data === "string") {
+        // wait for the job to complete
+        await waitForJob(fetchedData);
+      }
+      alert(fetchedData);   
     },
   },
 });
